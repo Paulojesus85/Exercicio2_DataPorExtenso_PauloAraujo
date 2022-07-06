92a61c7af460b3711ba7c2d70cddcedaa10c7c15
Paulojesus85 <paulo.historia.usp@gmail.com>
2022-07-06 10:59:11 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2_DataPorExtenso_PauloAraujo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
